ff754c0ac67ca5954717795e978bc4a9ceccaf9d
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2017-05-19 23:37:31 +0500

    MK: #4 resolved; User is allowed to open a form by clicking button on custom activity

--- odk/src/main/java/org/opensrp/form/odk/OdkLauncher.java
+++ odk/src/main/java/org/opensrp/form/odk/OdkLauncher.java
@@ -4,25 +4,69 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import org.odk.collect.android.activities.MainMenuActivity;
 
-// This class is main activity which is replacing
-// org.odk.collect.android.activities.SplashScreenActivity i.e. launcher of ODK
-// If code is upgraded make sure to setup everything here again which is done in mentioned activity in ODK
-// to make sure that nothing is missed out which is required for ODK
+/**
+ * Created by Maimoona on 5/19/2017.
+ *
+ * This class is main activity which is replacing org.odk.collect.android.activities.SplashScreenActivity
+ * i.e. launcher of ODK. If code is upgraded make sure to setup everything here again which is done
+ * in mentioned activity in ODK to make sure that nothing is missed out which is required for ODK-collect
+*/
 public class OdkLauncher extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // setup ODK whenever application is launched. This should cover everything ODK does in its init Activities
+        // setup ODK whenever application is launched. This should cover everything ODK-collect does
+        // in its init activities like main SplashScreenActivity, then MainActivity etc
         OdkUtils.setupODK(this);
+
+        // todo https://github.com/OpenSRP/form-render/issues/17
+        // download files and save those to odk-collect path and to odk-collect forms db as well
+        // the code odk-collect should be used as much as possible to prevent possibility of deviation
+        // incase odk-collect is upgraded
+        // The org.odk.collect.android.tasks.DiskSyncTask dos it automatically i.e. sync data according
+        // to form files existing in odk-collect forms folder. This may reduce work to manage FormDAO on our own
+        
+        // todo 'View Sent Form' option can be provided to users to help troubleshoot issues
+        // though its not needed. Also user app should be able to decide whether to see the option to
+        // view to delete forms after sending
+
+        // todo the application should let user app decide how to manage finalized, editable forms
+
+        // todo A blank form filled is identified by path+datetime. there is no oher id associated. the displayName field which
+        // user can input. when launching/saving form this field can be utilized to manage editable/finlaized form life cycle
+
+        // todo a way to change settings dynamically is to add settings to a file collect.settings which can be helpful in
+        // changing these dynamically from web dashboard. how this file should look like
+
+        // todo onListItemClick in org.odk.collect.android.activities.FormChooserList shows the way odk-collect
+        // handle forms. when user clicks item it fetches form URI by form id and opens
+        // org.odk.collect.android.activities.FormEntryActivity with extras
+        // we need to fetch form by formName or identifier or display and get the form long id from db
+        // and pass it to activity with content provider path or show error if none exists
 
         setContentView(R.layout.odk_launcher);
 
+        final Activity this_ = this;
+
+        ((Button)findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    OdkUtils.launchODKForm("Birds", this_);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         // launch new activity and close splash screen
-        startActivity(new Intent(this, MainMenuActivity.class));
-        finish();
+        // startActivity(new Intent(this, MainMenuActivity.class));
+        // finish();
     }
 }
--- odk/src/main/java/org/opensrp/form/odk/OdkUtils.java
+++ odk/src/main/java/org/opensrp/form/odk/OdkUtils.java
@@ -1,25 +1,45 @@
 package org.opensrp.form.odk;
 
+import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
 
 import org.odk.collect.android.application.Collect;
+import org.odk.collect.android.dao.FormsDao;
+import org.odk.collect.android.listeners.DiskSyncListener;
+import org.odk.collect.android.preferences.AdminPreferencesActivity;
 import org.odk.collect.android.preferences.PreferenceKeys;
+import org.odk.collect.android.provider.FormsProviderAPI;
+import org.odk.collect.android.tasks.DiskSyncTask;
+import org.odk.collect.android.utilities.ApplicationConstants;
+import org.odk.collect.android.utilities.ToastUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Map;
 
 import timber.log.Timber;
 
 /**
  * Created by Maimoona on 5/19/2017.
  */
-
 public class OdkUtils {
 
     // todo override some preferences
     // setup ODK whenever application is launched. This should cover everything ODK does in its init Activities
-    public static void setupODK(Context context){
+    public static void setupODK(Activity context){
         // must be at the beginning of any activity that can be called from an external intent
         Collect.createODKDirs();
 
@@ -46,6 +66,149 @@ public class OdkUtils {
         if (firstRun) {
             editor.putBoolean(PreferenceKeys.KEY_FIRST_RUN, false);
             editor.commit();
+
+            // don`t allow deleting forms (submitted/downloaded/blank)
+            boolean delete_saved = sharedPreferences.getBoolean("delete_saved", true);
+            if (delete_saved) {
+                editor.putBoolean("delete_saved", false);
+                editor.commit();
+            }
         }
+
+        // as specified in MainActivity of ODK-collect the settings should be updatable via collect.settings
+        // to override settings again, this file should be resent and pasted.
+        // This code after loading and updating settings deletes this file.
+        File f = new File(Collect.ODK_ROOT + "/collect.settings");
+        if (f.exists()) {
+            boolean success = loadSharedPreferencesFromFile(f);
+            if (success) {
+                ToastUtils.showLongToast(org.odk.collect.android.R.string.settings_successfully_loaded_file_notification);
+                f.delete();
+            } else {
+                ToastUtils.showLongToast(org.odk.collect.android.R.string.corrupt_settings_file_notification);
+            }
+        }
+
+        // This flag must be set each time the app starts up
+        setupGoogleAnalytics();
+
+        // must run DiskFormLoader
+        DiskSyncTask mDiskSyncTask = (DiskSyncTask) context.getLastNonConfigurationInstance();
+        if (mDiskSyncTask == null) {
+            Timber.i("Starting new disk sync task");
+            mDiskSyncTask = new DiskSyncTask();
+
+            // todo see where and how it update app about sync; Also every time forms are updated it should run itself;
+            // or if app runs first time
+            mDiskSyncTask.setDiskSyncListener(new DiskSyncListener() {
+                @Override
+                public void syncComplete(String result) {
+                    Log.v(getClass().getName(), "DiskSync COMPLETED "+result);
+                }
+            });
+            mDiskSyncTask.execute((Void[]) null);
+        }
+    }
+
+    // onListItemClick of org.odk.collect.android.activities.FormChooserList explains how form is loaded
+    public static void launchODKForm(long id, Activity context){
+        Uri formUri = ContentUris.withAppendedId(FormsProviderAPI.FormsColumns.CONTENT_URI, id);
+
+        Log.v(OdkUtils.class.getName(), "Launching URI "+formUri);
+
+        Intent intent = new Intent(Intent.ACTION_EDIT, formUri);
+        intent.putExtra(ApplicationConstants.BundleKeys.FORM_MODE, ApplicationConstants.FormModes.EDIT_SAVED);
+        context.startActivity(intent);
+    }
+
+    public static void launchODKForm(String formId, Activity context) throws IllegalAccessException {
+        Cursor formcursor = new FormsDao().getFormsCursorForFormId(formId);
+        try {
+            if (formcursor.getCount() > 0) {
+                formcursor.moveToFirst();
+
+                launchODKForm(formcursor.getLong(formcursor.getColumnIndex(FormsProviderAPI.FormsColumns._ID)), context);
+            }
+            else throw new IllegalAccessException("Form with given id ("+formId+") not exists. Make sure application life cycle is managed properly");
+        }
+        finally {
+            formcursor.close();
+        }
+    }
+
+    private static boolean loadSharedPreferencesFromFile(File src) {
+        // this should probably be in a thread if it ever gets big
+        boolean res = false;
+        ObjectInputStream input = null;
+        try {
+            input = new ObjectInputStream(new FileInputStream(src));
+            SharedPreferences.Editor prefEdit = PreferenceManager.getDefaultSharedPreferences(Collect.getInstance()).edit();
+            prefEdit.clear();
+            // first object is preferences
+            Map<String, ?> entries = (Map<String, ?>) input.readObject();
+            for (Map.Entry<String, ?> entry : entries.entrySet()) {
+                Object v = entry.getValue();
+                String key = entry.getKey();
+
+                if (v instanceof Boolean) {
+                    prefEdit.putBoolean(key, ((Boolean) v).booleanValue());
+                } else if (v instanceof Float) {
+                    prefEdit.putFloat(key, ((Float) v).floatValue());
+                } else if (v instanceof Integer) {
+                    prefEdit.putInt(key, ((Integer) v).intValue());
+                } else if (v instanceof Long) {
+                    prefEdit.putLong(key, ((Long) v).longValue());
+                } else if (v instanceof String) {
+                    prefEdit.putString(key, ((String) v));
+                }
+            }
+            prefEdit.apply();
+
+            // second object is admin options
+            SharedPreferences.Editor adminEdit = Collect.getInstance().getSharedPreferences(AdminPreferencesActivity.ADMIN_PREFERENCES,
+                    0).edit();
+            adminEdit.clear();
+            // first object is preferences
+            Map<String, ?> adminEntries = (Map<String, ?>) input.readObject();
+            for (Map.Entry<String, ?> entry : adminEntries.entrySet()) {
+                Object v = entry.getValue();
+                String key = entry.getKey();
+
+                if (v instanceof Boolean) {
+                    adminEdit.putBoolean(key, ((Boolean) v).booleanValue());
+                } else if (v instanceof Float) {
+                    adminEdit.putFloat(key, ((Float) v).floatValue());
+                } else if (v instanceof Integer) {
+                    adminEdit.putInt(key, ((Integer) v).intValue());
+                } else if (v instanceof Long) {
+                    adminEdit.putLong(key, ((Long) v).longValue());
+                } else if (v instanceof String) {
+                    adminEdit.putString(key, ((String) v));
+                }
+            }
+            adminEdit.apply();
+
+            res = true;
+        } catch (IOException | ClassNotFoundException e) {
+            Timber.e(e, "Exception while loading preferences from file due to : %s ", e.getMessage());
+        } finally {
+            try {
+                if (input != null) {
+                    input.close();
+                }
+            } catch (IOException ex) {
+                Timber.e(ex, "Exception thrown while closing an input stream due to: %s ", ex.getMessage());
+            }
+        }
+        return res;
+    }
+
+    // This flag must be set each time the app starts up
+    private static void setupGoogleAnalytics() {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(Collect
+                .getInstance());
+        boolean isAnalyticsEnabled = settings.getBoolean(PreferenceKeys.KEY_ANALYTICS, true);
+        GoogleAnalytics googleAnalytics = GoogleAnalytics.getInstance(Collect.getInstance());
+        googleAnalytics.setAppOptOut(!isAnalyticsEnabled);
     }
 }
